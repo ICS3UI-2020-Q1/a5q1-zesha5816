4b38a1d088bf1c8f872902f91627fcf48e982a6e
zesha5816 <71140785+zesha5816@users.noreply.github.com>
2020-10-06 10:23:16 -0400

    Update Main.java
    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Create a program that will ask the user for an integer. It will then create a       “multiplication table” for that number from 1 to 12.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//output message to user
+		System.out.println("Please enter an integer to create a multiplication table for");
+		//initialize the variables
+		int mulitplicationInt = input.nextInt();
+		int count=0;
+		int answer;
+		//only works while count is less than 12 
+		while (count < 12){
+			//add 1 to count everytime the loop repeats
+			count++;
+			//answer is count multypilying by the input value 
+			answer = count * mulitplicationInt;
+			//outputs the timetable 
+			System.out.println(count + " x " + mulitplicationInt + " = " + answer);
+			
+		}
   }
 }
